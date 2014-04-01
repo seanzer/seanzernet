a37a8a7df5887120380a5cc6b330d54e7bfb30b1
Sean Lee <SeanHsiLee@outlook.com>
2014-04-01 16:42:40 -0700

    Initial Website Commit

---
+++ GameDemo/pages/playcraftGame/playcraftGame.js
@@ -0,0 +1,32 @@
+﻿// For an introduction to the Page Control template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkId=232511
+(function () {
+    "use strict";
+
+    function loadGame() {
+        pc.start('pcGameCanvas', 'TheGame', 'js/', ['angrycannons.js'], '../../playcraftjs/lib/');
+    }
+
+    if (window.WinJS) {
+        WinJS.UI.Pages.define("/pages/playcraftGame/playcraftGame.html", {
+            // This function is called whenever a user navigates to this page. It
+            // populates the page elements with the app's data.
+            ready: function (element, options) {
+                // TODO: Initialize the page here.
+                loadGame();
+            },
+
+            unload: function () {
+                // TODO: Respond to navigations away from this page.
+            },
+
+            updateLayout: function (element) {
+                /// <param name="element" domElement="true" />
+
+                // TODO: Respond to changes in layout.
+            }
+        });
+    } else {
+        document.addEventListener("DOMContentLoaded", loadGame);
+    }
+})();
